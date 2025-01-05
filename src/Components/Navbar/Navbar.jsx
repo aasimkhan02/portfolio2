0f97981ee4a66ef209a77f771b107fd3b40c07f7
aasimkhan02 <aasimkroc@gmail.com>
2025-01-05 19:02:04 +0530

    New update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,14 +6,14 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className='Navbar'>
-            <h2 className='logo' style={{color: '#BEB7A4'}}>Aasim</h2>
+            <h2 className='logo' style={{color: 'black'}}>Aasim</h2>
             <div className="Nav-list">
                 <h4>Home</h4>
                 <h4>About</h4>
                 <h4>Skills</h4>
                 <h4>Projects</h4>
             </div>
-            <div className="Resume" style={{color: '#BEB7A4'}}>
+            <div className="Resume" style={{color: 'black'}}>
                 <h2>Resume <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="arrow-icon" /></h2>
             </div>
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
-import Frame1Video from './../../assets/Frame1.mp4';
 import '@lottiefiles/lottie-player';
+import HealthWave from './../../assets/Doctor.jpg'
+import AXWeather from './../../assets/Weather.jpg'
+import QuizElite from './../../assets/Quiz.jpg'
+import Frame1Video from './../../assets/Background new.mp4'
 
 const Home = () => {
     const [hoveredIndex, setHoveredIndex] = useState(null);
@@ -44,17 +47,10 @@ const Home = () => {
 
     return (
         <div className="Home">
-            <video className="Bg-video" autoPlay loop muted>
-                <source src={Frame1Video} type="video/mp4" />
-            </video>
             <div className="Hero">
-                <h4 style={{ width: '100%', textAlign: 'Left', paddingLeft: '60px' }}>I &nbsp;AM</h4>
-                <h1 style={{ width: '100%', textAlign: 'center', fontSize: '150px' }}>Aasim Khan</h1>
-                <h3 style={{
-                    textTransform: 'uppercase', wordSpacing: '5px', width: '100%',
-                    textAlign: 'right', paddingRight: '60px', color: '#eb5939',
-                    fontSize: '24px', letterSpacing: 1
-                }}>
+                <h4>I &nbsp;AM</h4>
+                <h1>Aasim Khan</h1>
+                <h3>
                     Crafting Code, Shaping Ideas
                 </h3>
             </div>
@@ -76,10 +72,10 @@ const Home = () => {
                 </div>
                 <div className="Main-content">
                     {[
-                        { skill: 'Programming Languages', details: ['Proficient in multiple programming languages with strong problem-solving skills to build efficient solutions.'], techstack: "Python, C, Java, Javascript" },
                         { skill: 'Frontend Development', details: ['Proficient in creating dynamic, user-friendly interfaces with modern frontend technologies.'], techstack: "HTML, CSS, Javascript, React, Bootstrap" },
-                        { skill: 'Backend Development', details: ['Experienced in building scalable backend systems and working with databases and APIs.'], techstack: "Django, MySQL, MySQLite" },
+                        { skill: 'Backend Development', details: ['Experienced in building scalable backend systems and working with databases and APIs.'], techstack: "Django, MySQL, SQLite" },
                         { skill: 'Machine Learning', details: ['Skilled in developing machine learning models and optimizing algorithms for predictive analysis.'], techstack: "Numpy, Pandas, Scikit-learn, Matplotlib" },
+                        { skill: 'Programming', details: ['Proficient in multiple programming languages with strong problem-solving skills to build efficient solutions.'], techstack: "Python, C, Java, Javascript" },
                         { skill: 'Tools', details: ['Proficient with tools that enhance productivity, version control, and deployment automation.'], techstack: "Figma, Jupyter Notebooks, Git, VS code" },
                     ].map((item, index) => (
                         <li
@@ -125,32 +121,84 @@ const Home = () => {
                     <h1>My Projects</h1>
                 </div>
                 <div className="Main-content">
-                    <div className="Healthcare">
-                        <div className="Project-image"></div>
-                        <div className="Project-content">
-                            <h1 style={{ fontSize: '55px', color: 'white' }}>HealthWave</h1>
-                            <h3 style={{ fontSize: '24px', color: 'rgb(180, 180, 180)' }}>
-                                Personal Project <span style={{ margin: '0 8px' }}>•</span> 2024
-                            </h3>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="Weather">
-                            <div className="Project-image"></div>
-                            <div className="Project-content">
-                                <h1 style={{ color: 'black', fontSize: 40 }}>AXWeather</h1>
-                                <h3 style={{ fontSize: 20 }}>
-                                    Personal Project <span style={{ margin: '0 8px' }}>•</span> 2023
-                                </h3>
+                    <div className="Healthwave">
+                        <img src={HealthWave} alt="" />
+                        <div className="project-details">
+                            <div className="project-title">
+                                <p><span style={{fontWeight: "bold"}}>HealthWave </span>- A cutting-edge blend of health education,
+                                AI predictions, and medication insights, redefining your well-being.</p>
+                                <h2 style={{fontWeight: 400, color: '#717171'}}>Personal Project • 2024</h2>
+                            </div>
+                            <div className="project-description">
+                                <p style={{fontSize: 21, color: '#717171'}}>HealthWave is a comprehensive platform designed to help users navigate their health journey 
+                                    with ease. It combines detailed medication information, health prediction models, 
+                                    and educational videos to offer valuable insights into managing and 
+                                    understanding various health conditions. By providing accessible resources 
+                                    and tools, HealthWave aims to empower users to make informed decisions and 
+                                    take proactive steps in maintaining their well-being.
+                                </p>
+                                <ul className="project-techstack">
+                                    <li>React</li>
+                                    <li>Django</li>
+                                    <li>SQLite</li>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                </ul>
                             </div>
                         </div>
-                        <div className="Quiz">
-                            <div className="Project-image"></div>
-                            <div className="Project-content">
-                                <h1 style={{ color: 'white', fontSize: 40 }}>QuizElite</h1>
-                                <h3 style={{ fontSize: 20 }}>
-                                    Personal Project <span style={{ margin: '0 8px' }}>•</span> 2023
-                                </h3>
+                    </div>
+                    <div className="AXWeather">
+                        <img src={AXWeather} alt="" />
+                        <div className="project-details">
+                            <div className="project-title">
+                                <p><span style={{fontWeight: "bold"}}>AXWeather </span>- A dynamic platform offering 
+                                accurate forecasts and interactive visualizations, empowering you to stay ahead of the weather.</p>
+                                <h2 style={{fontWeight: 400, color: '#3e3e3e'}}>Personal Project • 2023</h2>
+                            </div>
+                            <div className="project-description">
+                                <p style={{fontSize: 21, color: '#3e3e3e'}}>AXWeather is a versatile platform 
+                                    crafted to keep users informed and ready for any weather conditions. 
+                                    It provides precise forecasts and interactive tools to visualize 
+                                    atmospheric patterns, ensuring a clear understanding of weather dynamics. 
+                                    With its accessible and timely insights, AXWeather empowers users to plan 
+                                    ahead, adapt to changes, and confidently navigate their day, no matter the forecast.
+                                </p>
+                                <ul className="project-techstack">
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>JAVASCRIPT</li>
+                                    <li>API Integration</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="QuizElite">
+                        <img src={QuizElite} alt="" />
+                        <div className="project-details">
+                            <div className="project-title">
+                                <p><span style={{fontWeight: "bold"}}>QuizElite </span>- 
+                                A dynamic hub for engaging quizzes, seamless hosting, and collaborative 
+                                learning, transforming how you test and expand your knowledge.</p>
+                                <h2 style={{fontWeight: 400, color: '#555555'}}>Personal Project • 2023</h2>
+                            </div>
+                            <div className="project-description">
+                                <p style={{fontSize: 21, color: '#555555'}}>QuizElite is a versatile 
+                                    platform designed to make learning and engagement both fun and 
+                                    interactive. It offers a diverse range of quizzes across various 
+                                    topics, along with tools to host your own or join others effortlessly.
+                                    By fostering collaboration and challenge, QuizElite encourages 
+                                    users to test their knowledge, share ideas, and connect with 
+                                    others in an interactive learning environment. With its 
+                                    accessible features, QuizElite transforms traditional 
+                                    quizzing into an engaging and social experience for everyone.
+                                </p>
+                                <ul className="project-techstack">
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>Javascript</li>
+                                    <li>Django</li>
+                                    <li>Authentication</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
